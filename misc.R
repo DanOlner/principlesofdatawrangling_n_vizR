0e3054a19a1b206c7d8fc2b13f25da76b7542b06
Dan Olner <danolner@gmail.com>
2021-09-23 10:25:05 +0100

    r intro update

--- misc.R
+++ misc.R
@@ -12,6 +12,10 @@ ggplot(df, aes(x = price)) +
   coord_cartesian(xlim = c(1e+04,1e+07))
 
 
+qqnorm(df$price,main="QQ plot of normal data",pch=19) 
+#Add a line where x = y to help assess how closely the scatter fits the line.   
+qqline(normal)
+
 plot(density(rnorm(n = 1000000, mean = 0, sd = 1)), zero.line = T)
 
 plot(density(rnorm(n = 19, mean = 0, sd = 1)), zero.line = T)
